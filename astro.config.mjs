3e9093f30e34dae1e875f4423aec22c99bf9730d
John Muhlenberg <jmuhlenberg16@yahoo.com>
2026-03-27 11:40:57 -0600

    optimizing SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://jmuhlenberg.github.io',
   base: '/portfolio-2026/',
+  integrations: [sitemap()],
 });
